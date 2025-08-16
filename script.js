c00c76888b0e53bb81de00d03507f08554af2a5d
noName <132767802+noNameEXP@users.noreply.github.com>
2025-08-16 22:34:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ fetch('/api/log').catch(() => {});
 
 setTimeout(() => {
   window.location.href = 'https://www.google.com/search?q=watermelon&tbm=isch';
-  alert("Downloading zip-bomb.zip 0/1,000 GB");
+  //alert("Downloading zip-bomb.zip 0/1,000 GB");
 }, 1000);
